a086ab25cf2efd2cea7e41d3e1672250510058d3
Wesley <Loermansw@gmail.com>
2020-02-01 12:21:00 +0100

    Player Jump improvement, raycast bug fix.

--- Assets/PlayerController2D.cs
+++ Assets/PlayerController2D.cs
@@ -8,7 +8,7 @@ public class PlayerController2D : MonoBehaviour
 
     public float speed = 10.0f;
 
-    private float jumpTimer = 1;
+    public float jumpForce = 10;
 
     private CapsuleCollider2D CC2d;
 
@@ -17,6 +17,7 @@ public class PlayerController2D : MonoBehaviour
     // 1 == right.
 
     private RaycastHit2D hit;
+    private RaycastHit2D interact;
 
     private GameObject target;
 
@@ -28,15 +29,7 @@ public class PlayerController2D : MonoBehaviour
     [SerializeField]
     private bool grounded = true;
 
-    [SerializeField]
-    private float fallSpeed = -5;
-    
-    [SerializeField]
-    private AnimationCurve jumpCurve;
-
-    [SerializeField]
-    private float jumpTimerSpeed = 0.001f;
-
+    // This is to check if the vector varToMask is between minMax & -minMax.
     bool vector2Mask(Vector2 varToMask, float minMax){
         if(varToMask.x < minMax && varToMask.x > -minMax && varToMask.y < minMax && varToMask.y > -minMax){
             return true;
@@ -45,26 +38,20 @@ public class PlayerController2D : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         CC2d = gameObject.GetComponent<CapsuleCollider2D>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        // This part is for jumping & moving.
         if(Input.GetAxis("Jump") != 0 && grounded == true){
-            jumpTimer = 0.0f;  
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
         }
 
-        jumpTimer += jumpTimerSpeed;
-        jumpTimer = Mathf.Clamp(jumpTimer, 0f, 1f);
-
-        fallSpeed = jumpCurve.Evaluate(jumpTimer);
-        
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, fallSpeed);        
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
     }
 
     void Update(){
@@ -77,11 +64,12 @@ public class PlayerController2D : MonoBehaviour
         
         }
         
+        // This is dedicated to getting and grabbing any object with the "Grabbable Object" tag.
         if(target == null){
             switch(lastDir){
                 case -1:
                     if(Input.GetAxis("Fire1") != 0){
-                        hit = Physics2D.Raycast(transform.position, Vector2.left);
+                        hit = Physics2D.Raycast(transform.position, Vector2.left, 1f);
                         Debug.DrawRay(transform.position, Vector2.left, Color.green);
                     }
 
@@ -92,10 +80,9 @@ public class PlayerController2D : MonoBehaviour
 
                 case 1:
                     if(Input.GetAxis("Fire1") != 0){
-                        hit = Physics2D.Raycast(transform.position, Vector2.right);
+                        hit = Physics2D.Raycast(transform.position, Vector2.right, 1f);
                         Debug.DrawRay(transform.position, Vector2.right, Color.green);
                     }
-
                     break;
             }
 
@@ -108,6 +95,7 @@ public class PlayerController2D : MonoBehaviour
             }
         }
 
+        // This is dedicated to throwing the target (object with "Grabbable Object" tag).
         if(target != null){
             target.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, 0);
 
@@ -118,10 +106,36 @@ public class PlayerController2D : MonoBehaviour
                 target = null;
             }
         }
+
+        // This is to make sure you don't immediately throw the item if you keep the button pressed.
         if(Input.GetAxis("Fire1") == 0){
             grabLock = false;
         }
 
+        // Here we handle "interact" button inputs.
+        if(Input.GetAxis("Fire2") != 0){
+            
+            // Get "interact through raycasts"
+            switch(lastDir){
+                case -1:
+                        interact = Physics2D.Raycast(transform.position, Vector2.left, 1f);
+                        Debug.DrawRay(transform.position, Vector2.left, Color.green);
+                    break;
+                
+                case 0:
+                    break;
+
+                case 1:
+                        interact = Physics2D.Raycast(transform.position, Vector2.right, 1f);
+                        Debug.DrawRay(transform.position, Vector2.right, Color.green);
+                    break;
+            }
+
+            // Use "interact" to call functions within a script that corresponds with the item used.
+            // interact.collider.GetComponent( SCRIPT GOES HERE).FUNCTION TO USE;
+        }
+
+        // This is for the animations.
         gameObject.GetComponent<Animator>().SetFloat("Direction", Input.GetAxis("Horizontal"));
 
         if(lastDir < 0){
@@ -135,8 +149,29 @@ public class PlayerController2D : MonoBehaviour
         } else {
             gameObject.GetComponent<Animator>().SetBool("Idle", false);
         }
+        
+        if(rb.velocity.x != 0 && grounded == true){
+            gameObject.GetComponent<Animator>().SetBool("Walking", true);
+        }
+
+        if (rb.velocity.x < 0.1 && rb.velocity.x > -0.1){
+            gameObject.GetComponent<Animator>().SetBool("Walking", false);
+        } else if (grounded == false){
+            gameObject.GetComponent<Animator>().SetBool("Walking", false);
+        }
+
+        if(rb.velocity.y > 0){
+            gameObject.GetComponent<Animator>().SetInteger("VerticalMomentum", 1);
+        } else if (rb.velocity.y < 0){
+            gameObject.GetComponent<Animator>().SetInteger("VerticalMomentum", -1);
+        }
+        
+        if (rb.velocity.y < 0.1 && rb.velocity.y > -0.1){
+            gameObject.GetComponent<Animator>().SetInteger("VerticalMomentum", 0);
+        }
     }
 
+    // Grounded detection.
     void OnTriggerStay2D(Collider2D col){
         grounded = true;
     }
