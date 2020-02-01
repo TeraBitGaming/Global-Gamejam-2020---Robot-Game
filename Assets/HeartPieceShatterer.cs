834507395b85ed49834ca44a1b4ff2d4d2f6fe95
Wesley <Loermansw@gmail.com>
2020-02-01 21:43:01 +0100

    Fixed issue with missing healthmanager
    bug fix

---
+++ Assets/HeartPieceShatterer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartPieceShatterer : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] heartPieces;
+
+    [SerializeField]
+    private ParticleSystem particleSystem;
+
+    public void shatter(){
+        gameObject.GetComponent<UnityEngine.UI.Image>().enabled = false;
+        particleSystem.Play();
+        for (int i = 0; i < heartPieces.Length; i++)
+        {
+            heartPieces[i].SetActive(true);
+            heartPieces[i].GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // shatter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
