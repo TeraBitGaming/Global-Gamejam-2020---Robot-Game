f8fb9a2e1055caccf352289fdef403c876c040a0
Wesley <Loermansw@gmail.com>
2020-02-02 10:37:03 +0100

    Changed heart, added camera.

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void loadScene(int sceneSelect){
+        SceneManager.LoadScene(sceneSelect);
+    }
+
+    public IEnumerator ReloadScene(float delay){
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(1);
+    }
+}
